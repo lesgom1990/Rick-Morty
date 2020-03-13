133389123972e4f117d6f4f604ee331317351de1
lesly <lesgom1990@gmail.com>
2020-03-12 19:08:16 -0500

    version 2, larga y poco optima

--- rickandmorty.js
+++ rickandmorty.js
@@ -5,21 +5,28 @@ let datarenderlocation ="";
 let datarendergender ="";
 let datarenderstatus ="";
 let datarenderspecies ="";
+let arrayimage;
+let arrayname;
+let arraylocation;
+let arraygender;
+let arraystatus;
+let arrayspecies;
+let informacionimage = document.getElementById("infoimage");
+let informacionname = document.getElementById("infoname");
+let informacionlocation = document.getElementById("infolocation");
+let informaciongender = document.getElementById("infogender");
+let informacionstatus = document.getElementById("infostatus");
+let informacionspecies = document.getElementById("infospecies");
 
 fetch(url)
 .then(response =>response.json())
 .then(info=>{
-        callcharacter(info); 
+        callcharacter(info);
 })
 .catch(err => console.log(err))
 
 let callcharacter = infocharacter=>{
-    let informacionimage = document.getElementById("infoimage");
-    let informacionname = document.getElementById("infoname");
-    let informacionlocation = document.getElementById("infolocation");
-    let informaciongender = document.getElementById("infogender");
-    let informacionstatus = document.getElementById("infostatus");
-    let informacionspecies = document.getElementById("infospecies");
+    
     for (var i=0; i<infocharacter.results.length; i++){
         datarenderimage += `"<img src="${infocharacter.results[i].image}" width="300px">`
         datarendername +="<p class= info>Name = "+infocharacter.results[i].name;
@@ -28,25 +35,18 @@ let callcharacter = infocharacter=>{
         datarenderstatus +="<p class= info>Status = "+infocharacter.results[i].status;
         datarenderspecies +="<p class= info>Specie = "+infocharacter.results[i].species;
     }
-    console.log(datarenderimage);
     informacionimage.innerHTML += datarenderimage;
     informacionname.innerHTML += datarendername;
     informacionlocation.innerHTML += datarenderlocation;
     informaciongender.innerHTML += datarendergender;
     informacionstatus.innerHTML += datarenderstatus;
     informacionspecies.innerHTML += datarenderspecies;
+    console.log(informacionname);
+    var separador = "<p class= info>";
+    cambioStringToArray(datarendername, separador);
 }
 
-/* Código anterior
-let callcharacter = infocharacter=>{
-    let informacion = document.getElementById("info");
-    for (var i=0; i<infocharacter.results.length; i++){
-        informacion.innerHTML= `"<img src="${infocharacter.results[i].image}" width="300px">`
-        informacion.innerHTML+="<p class= info>Name = "+infocharacter.results[i].name;
-        informacion.innerHTML+="<p class= info> Location = "+infocharacter.results[i].location.name;
-        informacion.innerHTML+="<p class= info>Gender = "+infocharacter.results[i].gender;
-        informacion.innerHTML+="<p class= info>Status = "+infocharacter.results[i].status;
-        informacion.innerHTML+="<p class= info>Specie = "+infocharacter.results[i].species;
-    }
-    console.log(info);
-}*/
+function cambioStringToArray(str, expresion){
+var array = str.split(expresion);
+console.log(array);
+}
